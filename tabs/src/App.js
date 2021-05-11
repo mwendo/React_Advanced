c20c000540cc00895c43c40d06a0198d9ca604b9
Micah Wendorf <wendorfmicah0@gmail.com>
2021-05-11 16:22:41 -0500

    initial commit

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
 import './App.css';
-import Tabs from './components/Tabs';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Tabs from './components/Tabs';
+import {useState} from 'react';
 
 function App() {
+  const [tabs, setTabs] = useState([
+    {label: "jerry", content: "jerry's content here!", selected: true},
+    {label: "jerome", content: "jerome's e!", selected: false},
+    {label: "gerald", content: "gerald's pizza icontent here!", selected: false},
+    {label: "jason", content: "jason's content bad at bowling here!", selected: false}
+  ])
+
+  const onClickHandler = (selectedIndex) => {
+    console.log(tabs[selectedIndex].label)
+
+    tabs.map((tab, i ) => {
+      let t = tab;
+      if(i === selectedIndex){
+        t.selected = true;
+      }
+      else{
+        t.selected = false;
+      }
+      return setTabs([...tabs.slice(0,i), t, ...tabs.slice(i+1)]);
+    })
+  }
   return (
-    <div className="App">
-      <Tabs/>
+    <div>
+      {
+        tabs.map((item, i) => <Tabs label={item.label} selected={item.selected} onClickHandler={onClickHandler} index={i} key={i} />)
+      }
+
+      {
+        tabs.map((item, i) => {
+          if(item.selected){
+            return <h1 key={i}>{item.content}</h1>
+          }
+          else return null;
+        })
+      }
     </div>
   );
 }
--- tabs/src/components/Tabs.jsx
+++ tabs/src/components/Tabs.jsx
@@ -1,32 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Tabs = props => {
-
-    const[tabs, setTabs] = useState();
-
-    const tab = [
-        "Hello welcome to tab 1 boy!",
-        "What's up my man you're at tab 2, yo!",
-        "This is the depths of despair, also known as TAB 3!!!!"
-    ];
-
-    return(
-        <div class="container mt-5">
-            <h1>Tabs Assignment</h1>
-            <ul className="nav nav-tabs my-4">
-                <li className="nav-item">
-                    <a className={tabs === 0 ? "nav-link active": "nav-link"} href="/#" onClick={() => setTabs(0)}>Tab 1</a>
-                </li>
-                <li className="nav-item">
-                    <a className={tabs === 1 ? "nav-link active": "nav-link"} href="/#" onClick={() => setTabs(1)}>Tab 2</a>
-                </li>
-                <li className="nav-item">
-                    <a className={tabs === 2 ? "nav-link active": "nav-link"} href="/#" onClick={() => setTabs(2)}>Tab 3</a>
-                </li>
-            </ul>
-            <div className="alert">{tab[tabs]}</div>
+const Tabs = (props) => {
+    const selectedStyle = {
+        backgroundColor: "black",
+        color: "white",
+    }
+    return (
+        <div onClick={()=>props.onClickHandler(props.index)} className="border m-3 text-center" style={props.selected ? selectedStyle : {}}>
+            <h1>label: {props.label}</h1>
         </div>
-    )
-}
+    );
+};
+
 
 export default Tabs;
